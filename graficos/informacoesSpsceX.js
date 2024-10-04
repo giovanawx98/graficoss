a007f01ae9841a7c4b1d28d5cc36901bf9e9a3ea
Giovana <111807601+giovanawx98@users.noreply.github.com>
2024-10-03 21:58:45 -0300

    Update informacoesSpsceX.js

--- graficos/informacoesSpsceX.js
+++ graficos/informacoesSpsceX.js
@@ -1,19 +1,20 @@
-const url = 'https://api.spacexdata.com/v4/launches/latest';
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-async function vizualizarInformacoesSpaceX() {
-    const res = await fetch(url);
-    const dados = await res.json();
-    
-    const nomeMissao = dados.name;
-    const dataLancamento = new Date(dados.date_utc).toLocaleDateString();
-    const foguete = dados.rocket;
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas) * 100)
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
 
-    const paragrafo = document.createElement('p');
-    paragrafo.classList.add('graficos-container__texto');
-    paragrafo.innerHTML = `O lançamento mais recente da SpaceX foi a missão <span>${nomeMissao}</span> com o foguete <span>${foguete}</span>, que ocorreu em <span>${dataLancamento}</span>.`;
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
 
-    const container = document.getElementById('graficos-container');
-    container.appendChild(paragrafo);
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
 }
 
-vizualizarInformacoesSpaceX();
+vizualizarInformacoesGlobais()
