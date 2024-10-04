69a4f97a50696c4532a47e2f67a7428190afdc8c
Giovana <111807601+giovanawx98@users.noreply.github.com>
2024-10-03 22:05:56 -0300

    Update informacoesSpsceX.js

--- graficos/informacoesSpsceX.js
+++ graficos/informacoesSpsceX.js
@@ -1,4 +1,4 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = 'https://dog.ceo/api/breeds/list/all'
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
