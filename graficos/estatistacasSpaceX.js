f7cbad2fccb1c5220ff2b7183c201f14f4328230
Giovana <111807601+giovanawx98@users.noreply.github.com>
2024-10-03 22:28:21 -0300

    Update estatistacasSpaceX.js

--- graficos/estatistacasSpaceX.js
+++ graficos/estatistacasSpaceX.js
@@ -1,54 +1,55 @@
 import { getCSS, tickConfig } from "./common.js";
 
-async function quantidadeUsuariosPorRede() {
-    const url = 'https://dog.ceo/api/breeds/list/all';
+const url = 'https://pokeapi.co/api/v2/pokemon/ditto';
+
+async function estatisticasDitto() {
     const res = await fetch(url);
     const dados = await res.json();
-    const racas = Object.keys(dados.message);
-    const quantidadeRacas = racas.length;
 
-    // Gráfico de barras das raças (dividido em segmentos para visualização)
+    const estatisticas = dados.stats.map(stat => stat.base_stat);
+    const nomesEstatisticas = dados.stats.map(stat => stat.stat.name);
+
     const data = [
         {
-            x: racas.slice(0, 10), // Mostrando apenas as 10 primeiras raças
-            y: Array(10).fill(1), // Apenas para ilustrar a quantidade de raças
+            x: nomesEstatisticas,
+            y: estatisticas,
             type: 'bar',
             marker: {
-                color: getCSS('--primary-color'),
-            },
-        },
+                color: getCSS('--primary-color')
+            }
+        }
     ];
 
     const layout = {
         plot_bgcolor: getCSS('--bg-color'),
         paper_bgcolor: getCSS('--bg-color'),
         title: {
-            text: 'As primeiras 10 raças de cães',
+            text: 'Estatísticas do Pokémon Ditto',
             x: 0,
             font: {
                 color: getCSS('--primary-color'),
                 size: 30,
-                font: getCSS('--font'),
-            },
+                font: getCSS('--font')
+            }
         },
         xaxis: {
             tickfont: tickConfig,
             title: {
-                text: 'Raças',
+                text: 'Estatísticas',
                 font: {
-                    color: getCSS('--secondary-color'),
-                },
-            },
+                    color: getCSS('--secondary-color')
+                }
+            }
         },
         yaxis: {
             tickfont: tickConfig,
             title: {
-                text: 'Quantidade',
+                text: 'Valor Base',
                 font: {
-                    color: getCSS('--secondary-color'),
-                },
-            },
-        },
+                    color: getCSS('--secondary-color')
+                }
+            }
+        }
     };
 
     const grafico = document.createElement('div');
@@ -57,4 +58,4 @@ async function quantidadeUsuariosPorRede() {
     Plotly.newPlot(grafico, data, layout);
 }
 
-quantidadeUsuariosPorRede();
+estatisticasDitto();
