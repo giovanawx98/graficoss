dbc1b55379d9985c5e3af00296a11472dc1a4498
Giovana <111807601+giovanawx98@users.noreply.github.com>
2024-10-03 22:02:39 -0300

    Update estatistacasSpaceX.js

--- graficos/estatistacasSpaceX.js
+++ graficos/estatistacasSpaceX.js
@@ -1,51 +1,28 @@
-import { getCSS, tickConfig } from "./common.js";
+import { getCSS, tickConfig } from "./common.js"
 
-const launchesUrl = 'https://api.spacexdata.com/v4/launches';
-const rocketsUrl = 'https://api.spacexdata.com/v4/rockets';
-
-async function pegarNomeFoguetes() {
-    const res = await fetch(rocketsUrl);
-    const dados = await res.json();
-    
-    const foguetes = {};
-    dados.forEach(foguete => {
-        foguetes[foguete.id] = foguete.name;
-    });
-    
-    return foguetes;
-}
-
-async function estatisticasSpaceX() {
-    const res = await fetch(launchesUrl);
-    const dados = await res.json();
-
-    const foguetesNomes = await pegarNomeFoguetes();
-
-    const foguetesContagem = {};
-    dados.forEach(launch => {
-        const foguete = foguetesNomes[launch.rocket]; 
-        foguetesContagem[foguete] = (foguetesContagem[foguete] || 0) + 1;
-    });
-
-    const nomeFoguetes = Object.keys(foguetesContagem);
-    const quantidadeLançamentos = Object.values(foguetesContagem);
+async function quantidadeUsuariosPorRede() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeDeUsuarios = Object.values(dados)
 
     const data = [
         {
-            x: nomeFoguetes, 
-            y: quantidadeLançamentos, 
+            x: nomeDasRedes, 
+            y: quantidadeDeUsuarios, 
             type: 'bar',
             marker: {
                 color: getCSS('--primary-color')
             }
         }
-    ];
+    ]
 
-    const layout = {
+    const laytout = {
         plot_bgcolor: getCSS('--bg-color'),
         paper_bgcolor: getCSS('--bg-color'),
         title: {
-            text: 'Lançamentos por Tipo de Foguete',
+            text: 'Redes sociais com mais usuários',
             x: 0,
             font: {
                 color: getCSS('--primary-color'),
@@ -56,7 +33,7 @@ async function estatisticasSpaceX() {
         xaxis: {
             tickfont: tickConfig,
             title: {
-                text: 'Foguetes',
+                text: 'Nome das redes',
                 font: {
                     color: getCSS('--secondary-color')
                 }
@@ -65,18 +42,18 @@ async function estatisticasSpaceX() {
         yaxis: {
             tickfont: tickConfig,
             title: {
-                text: 'Número de Lançamentos',
+                text: 'Bilhões de usuários ativos',
                 font: {
                     color: getCSS('--secondary-color')
                 }
             }
         }
-    };
+    }
 
-    const grafico = document.createElement('div');
-    grafico.className = 'grafico'; 
-    document.getElementById('graficos-container').appendChild(grafico); 
-    Plotly.newPlot(grafico, data, layout); 
+    const grafico = document.createElement('div')
+    grafico.className = 'grafico'
+    document.getElementById('graficos-container').appendChild(grafico)
+    Plotly.newPlot(grafico, data, laytout)
 }
 
-estatisticasSpaceX();
+quantidadeUsuariosPorRede(
